880ff4d5729aa2e03c5299c0e769114fc67f4791
Joshua Sani <joshuasani@hotmail.com>
2017-02-10 15:37:58 +0900

    Hello Team 1.

---
+++ Lolcats/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lolcats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeowMeow();
+        }
+
+        static void MeowMeow()
+        {
+            // Stanford please make this function say "meow meow".
+        }
+    }
+}
